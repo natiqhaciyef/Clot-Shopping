7af77c6fcccbcb40b19b2d22e47c6dc21bae55f2
natiqhaciyef <natiq00h2272@gmail.com>
2023-05-11 17:06:09 +0400

    UI - Theme and App main colors added

--- app/src/main/java/com/natiqhaciyef/clot_mobile/MainActivity.kt
+++ app/src/main/java/com/natiqhaciyef/clot_mobile/MainActivity.kt
@@ -2,10 +2,15 @@ package com.natiqhaciyef.clot_mobile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import com.natiqhaciyef.clot_mobile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
