534800132375b7119b4900c80f2b54875c0a5862
natiqhaciyef <natiq00h2272@gmail.com>
2023-05-14 00:56:20 +0400

    UI - Main UIs created and HomeScreen top view (dropdown menu styling) created

---
+++ app/src/main/java/com/natiqhaciyef/clot_mobile/view/screen/home/fragment/NotificationFragment.kt
@@ -0,0 +1,27 @@
+package com.natiqhaciyef.clot_mobile.view.screen.home.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.natiqhaciyef.clot_mobile.R
+import com.natiqhaciyef.clot_mobile.databinding.FragmentNotificationBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class NotificationFragment : Fragment() {
+    private lateinit var binding: FragmentNotificationBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNotificationBinding.inflate(inflater, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
