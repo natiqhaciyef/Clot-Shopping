ce23ddbbdc2151b3f383974bc838cc28ec6ed3a6
natiqhaciyef <natiq00h2272@gmail.com>
2023-05-14 21:56:37 +0400

    UI - Product Recyclerview and adapter created.

--- app/src/main/java/com/natiqhaciyef/clot_mobile/view/screen/home/fragment/HomeFragment.kt
+++ app/src/main/java/com/natiqhaciyef/clot_mobile/view/screen/home/fragment/HomeFragment.kt
@@ -6,13 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.natiqhaciyef.clot_mobile.R
+import com.natiqhaciyef.clot_mobile.data.models.ProductModel
 import com.natiqhaciyef.clot_mobile.databinding.FragmentHomeBinding
+import com.natiqhaciyef.clot_mobile.view.adapter.ProductAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var productAdapter: ProductAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,8 +28,16 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val categories = mutableListOf<String>("Women", "Men", "Kids", "Unisex")
-        val adapter = ArrayAdapter<String>(requireContext(), R.layout.drop_down_list_item, categories)
+        val categories = mutableListOf("Women", "Men", "Kids", "Unisex")
+        val adapter = ArrayAdapter(requireContext(), R.layout.drop_down_list_item, categories)
         binding.dropDownListElement.setAdapter(adapter)
+
+//        productAdapter = ProductAdapter(requireContext(), productList)
+//        binding.recyclerViewProducts.adapter = productAdapter
+//        binding.recyclerViewProducts.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+//        productAdapter.onClick { productModel ->
+//            println(productModel)
+//        }
+
     }
 }
